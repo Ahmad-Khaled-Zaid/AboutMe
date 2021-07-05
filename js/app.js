90ed7826753d923de36d622d219a96701f5f0eda
Ahmad-Khaled-Zaid <ahmadk.zaid97@gmail.com>
2021-07-06 01:28:30 +0300

    edit js file

--- js/app.js
+++ js/app.js
@@ -11,14 +11,23 @@ if (ask == "yes" || ask == "y") {
     alert("amazing, you have to answer 5 questions with yes or no, you can use y or n inested ")
     let Q1 = prompt("is Peterson’s platform better than fast web?")
     // console.log(Q1);
+alert("good suggestion")
     let Q2 = prompt(" is Duct Tape Attire Scholarships the most valuable? ")
     // console.log(Q2);
+    alert("we thought that too")
+
     let Q3 = prompt(" europe is the best place to apply for scholerships? ")
     // console.log(Q3);
+    alert("good suggestion")
+
     let Q4 = prompt(" Arab countries are lack of scholerships? ")
     // console.log(Q4);
+    alert("That's true")
+
     let Q5 = prompt(" you've been applied for a scholership before? ")
     // console.log(Q5);
+    alert("good suggestion")
+
 }
 
 
