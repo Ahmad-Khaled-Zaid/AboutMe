c8ee49d165ba9dadb54847b1d34b2c8b63767d4f
Ahmad-Khaled-Zaid <ahmadk.zaid97@gmail.com>
2021-07-05 16:22:35 +0300

    upload new repo

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# AboutMe
+# LAB-02
+## code 201 course
+I build an app with a direct and powerful purpose. It does all of the things that it accomplishes well. I should describe the purpose and functionality so those that visit my README understand the app
+
---
+++ js/app.js
@@ -0,0 +1,37 @@
+'use strict';
+let getname = prompt("welcome to our website, may i have your name please?")
+alert("nice to see you here " + getname)
+    // console.log(getname);
+
+let ask = prompt("we want to take your opinion in scholerships " + getname + " ,if you accept to answer Press y, otherwise press n ")
+ask = ask.toLowerCase();
+if (ask == "yes" || ask == "y") {
+
+
+    alert("amazing, you have to answer 5 questions with yes or no, you can use y or n inested ")
+    let Q1 = prompt("is Peterson’s platform better than fast web?")
+    // console.log(Q1);
+    let Q2 = prompt(" is Duct Tape Attire Scholarships the most valuable? ")
+    // console.log(Q2);
+    let Q3 = prompt(" europe is the best place to apply for scholerships? ")
+    // console.log(Q3);
+    let Q4 = prompt(" Arab countries are lack of scholerships? ")
+    // console.log(Q4);
+    let Q5 = prompt(" you've been applied for a scholership before? ")
+    // console.log(Q5);
+}
+
+
+else if (ask == "no" || ask == "n") {
+
+
+    alert("don't worry, you might have a look on our website")
+    
+
+}
+else {
+    alert("invaild input,please enter y,yes,n,no")
+
+}
+
+
